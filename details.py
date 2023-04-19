897b1aba238e12e02bf966f33a829c7521dddb11
ajogb <123110918+ajogb@users.noreply.github.com>
2023-04-19 12:18:36 -0400

    prints id of argument

--- details.py
+++ details.py
@@ -4,4 +4,8 @@ import cgi
 import cgitb
 cgitb.enable()
 print("Content-Type: text/html\n\n")
+API_KEY = open("apikey.txt","r").readlines()[0]
 
+args=cgi.parse()
+placeID = args["placeID"][0]
+print(placeID)
