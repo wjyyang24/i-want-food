60e8b5caca12cf1e942de54dee47236349238a75
ajogb <123110918+ajogb@users.noreply.github.com>
2023-04-25 17:09:23 -0400

    initial implementation of details page

--- website/details.py
+++ website/details.py
@@ -6,15 +6,16 @@ import cgitb
 def placeDetails(placeID):
     url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={placeID}&key={API_KEY}"
     response = requests.request("GET", url,)
-    results = json.loads(response.text)
+    results = response.text
 
     return results
 
 cgitb.enable()
-print("Content-Type: text/html\n\n")
+print("Content-Type: application/json\n\n")
 API_KEY = open("apiKey.txt","r").readlines()[0]
 
 args=cgi.parse()
 placeID = args["placeid"][0]
 
 details = placeDetails(placeID)
+print(details)
