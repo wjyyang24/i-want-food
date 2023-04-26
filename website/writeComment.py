939d0ff959f1f004202c9c76bbc72ae38a6c2b5b
ajogb <123110918+ajogb@users.noreply.github.com>
2023-04-25 20:32:08 -0400

    further work on comments

---
+++ website/writeComment.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.10
+from csv import reader
+import csv
+import cgi
+import cgitb
+import readComments
+FILENAME = "comments.csv"
+
+cgitb.enable()
+print("Content-Type: text/html\n\n")
+FILENAME = "comments.csv"
+args=cgi.parse()
+placeID = args["placeid"][0]
+review = args["review"][0]
+
+readComments.writeReview()
